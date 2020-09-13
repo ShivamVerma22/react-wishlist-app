0a32c3742e3784dafb186cb5c7b57f5c90c55ab0
Shivam Verma <vermashivam527@gmail.com>
2020-09-14 02:54:44 +0530

    props done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
 import React from "react";
 import "./styles.css";
+import getApiData from "./getApiData"
+
+const wishListStyle = {
+  padding: "10px",
+  border: "1px solid black",
+  background: "#f2f2f2",
+  marginBottom: "10px",
+  borderRadius: "5px",
+}
+
+function WishListItem({ item }){
+  console.log(item)
+  const { description, link } = item
+  return (
+    <div style={wishListStyle}>
+      <span>{description} </span>
+      <a href={link}>Buy</a>
+    </div>
+  )
+}
+
+function processList(wishListItem){
+  return <WishListItem item={wishListItem} key={wishListItem.id}/>
+}
+
+function WishList({ wishList }) {
+  console.log(wishList)
+  return wishList.map(processList)
+}
 
 export default function App() {
+  const data = getApiData();
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>{data.appName}</h1>
+      <h2>{data.userName}</h2>
+      <WishList wishList={data.wishList} />
     </div>
   );
 }
---
+++ src/getApiData.js
@@ -0,0 +1,20 @@
+function getApiData() {
+  return {
+    appName: "Wishlist App",
+    userName: "Shivam's Wishlist",
+    wishList: [
+      {
+        id: "1",
+        description: "Mobile Phone",
+        link: "www.something.com/mobile"
+      },
+      {
+        id: "2",
+        description: "VR Glasses",
+        link: "www.something.com/vr-glasses"
+      }
+    ]
+  };
+}
+
+export default getApiData;
